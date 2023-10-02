8388719d979980c44be53c96073a6f807ba89630
Tednn493 <Tednn493@gmail.com>
2023-10-02 20:15:51 +0100

    remade the grid in an easier format

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const container = document.querySelector(".container");
-
-function createGrid(rows,columns){
-    total= rows * (columns + 1) + 1
-    for (let c=0; c<total;c++){
-        let box= document.createElement("div");
-        box.className = "box";
-        box.innerText = c
-        container.appendChild(box);
-    };
+function makeGrid(size){
+    let board = document.querySelector(".board");
+    let boxes = board.querySelectorAll("div");
+    boxes.forEach((div) => div.remove())
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for ( let i=0 ; i< size*size ; i++){
+        let box = document.createElement('div');
+        box.addEventListener("mouseover",changeColor)
+        box.style.backgroundColor = ' white';
+        board.insertAdjacentElement('beforeend',box)
+    }
+ 
 }
 
-createGrid(16,16);
+function changeSize(size){
+    if (size>=4 && size <= 100){
+        makeGrid(size)
+    }else{
+        alert("Please ensure your input is between 4 and 100")
+    }
+}
+
+function changeColor(e){
+    this.style.backgroundColor="black"
+}
+
+
+makeGrid(16)
+
+
